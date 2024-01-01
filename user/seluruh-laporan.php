83629b58db326305c5048ed6a2ceb55758cff304
Muhammad Hafiz <ciafiz111@gmail.com>
2024-01-01 16:18:10 +0700

    update back to top

--- user/seluruh-laporan.php
+++ user/seluruh-laporan.php
@@ -312,7 +312,7 @@ $email = $_SESSION["user"];
     <!-- Footer End -->
 
     <!-- Back to top Start -->
-    <a href="#home"
+    <a href="#laporan"
         class="fixed bottom-4 right-4 z-[9999] hidden h-14 w-14 items-center justify-center rounded-full bg-primary p-4 hover:animate-pulse"
         id="to-top">
         <span class="mt-2 block h-5 w-5 rotate-45 border-t-2 border-l-2"></span>
