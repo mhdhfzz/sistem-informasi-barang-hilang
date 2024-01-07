16753d05f4928bd4ae825e01d75f54910afe39ee
nextz06 <ciafiz111@gmail.com>
2024-01-07 19:24:09 +0700

    delete command js

--- dist/js/script.js
+++ dist/js/script.js
@@ -78,23 +78,3 @@ accordionItems.forEach((item) => {
 		}
 	});
 });
-
-// Uploud Gambar
-document.getElementById("fileImg").onchange = function () {
-	document.getElementById("image").src = URL.createObjectURL(fileImg.files[0]); // Preview new image
-
-	document.getElementById("cancel").style.display = "block";
-	document.getElementById("confirm").style.display = "block";
-
-	document.getElementById("upload").style.display = "none";
-};
-
-var userImage = document.getElementById("image").src;
-document.getElementById("cancel").onclick = function () {
-	document.getElementById("image").src = userImage; // Back to previous image
-
-	document.getElementById("cancel").style.display = "none";
-	document.getElementById("confirm").style.display = "none";
-
-	document.getElementById("upload").style.display = "block";
-};
